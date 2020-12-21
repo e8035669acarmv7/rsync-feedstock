378100feccdbfbe993eba51ed24126347d48ca6d
Uwe L. Korn <uwe.korn@quantco.com>
2020-12-21 17:14:05 +0100

    Skip SIMD optimizations on osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,10 @@ set -e -o pipefail
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
-
-./configure --prefix=$PREFIX --without-included-zlib --without-included-popt
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    EXTRA_CONFIGURE_ARGS="--disable-simd"
+fi
+./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-}
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
     make check
